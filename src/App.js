5d2267cb8ee979df1ee0ba31ced942e79a4ee470
unknown <rele.varun1@gmail.com>
2022-12-26 17:27:38 +0530

    HTML changes

--- src/App.js
+++ src/App.js
@@ -13,23 +13,23 @@ const App = () => {
 
   return (
     <main className='h-fit w-full flex justify-center items-center sm:pt-14'>
-      <section className='bg-white w-full sm:w-[616px] h-full flex flex-col p-4 sm:rounded-lg md:shadow-md'>
+      <div className='bg-white w-full sm:w-[616px] h-full flex flex-col p-4 sm:rounded-lg md:shadow-md'>
 
         {/* Header */}
         <section className='flex justify-between items-center'>
-          <h1 className='flex font-extrabold items-center'>
+          <div className='flex font-extrabold items-center'>
             Notifications
             <div className='mx-2 text-sm font-bold px-2 text-white bg-blue-p rounded-md'>
             {notiCount}
           </div>
-          </h1>
+          </div>
           
           <p className='text-xs text-grayish-blue hover:cursor-pointer' onClick={markAllRead}>
             Mark all as read
           </p>
         </section>
 
-        <section className='flex flex-col'>
+        <div className='flex flex-col'>
           <Notification person={data.mark} unread={true} read={allRead} count={{notiCount, setNotiCount}} />
           <Notification person={data.angela} unread={true} read={allRead} count={{notiCount, setNotiCount}}  />
           <Notification person={data.jacob} unread={true}  read={allRead} count={{notiCount, setNotiCount}} />
@@ -37,10 +37,10 @@ const App = () => {
           <Notification person={data.kimberly} unread={false} read={allRead} count={{notiCount, setNotiCount}}  />
           <Notification person={data.nathan} unread={false} read={allRead} count={{notiCount, setNotiCount}}  />
           <Notification person={data.anna} unread={false} read={allRead} count={{notiCount, setNotiCount}} />
-        </section>
+        </div>
         
 
-      </section>
+      </div>
     </main>
   )
 }
